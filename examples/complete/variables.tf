2106cf48bf93eb2fcc73150a29c3e63443649fab
Igor Yakovlev <igory@checkpoint.com>
2021-10-19 15:36:40 +0300

    init

---
+++ examples/complete/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type = string
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = string
+  description = "Name (e.g. `app`)"
+}
+
+variable "cidr_block" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "Base CIDR block which is divided into subnet CIDR blocks (e.g. `10.0.0.0/16`)"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of Availability Zones (e.g. `['us-east-1a', 'us-east-1b', 'us-east-1c']`)"
+}
